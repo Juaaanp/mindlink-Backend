e4a9f31c006a1007e256383f5fb36fd1ff42d836
juaaanp <juanparodriguezg@gamil.com>
2025-03-30 23:15:00 -0500

    Inicializacion del backend de mindlink

---
+++ src/main/java/com/mindlink/MindlinkApplication.java
@@ -0,0 +1,13 @@
+package com.mindlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindlink/MindlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
